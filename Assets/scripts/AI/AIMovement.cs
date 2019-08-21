b89a03c70f7546ed2e5cba89bdc9a83439e2e43e
Cary Small <carysmal@amazon.com>
2019-08-21 18:31:15 +0200

    refactored adding an abstract class for movement

--- Assets/scripts/AI/AIMovement.cs
+++ Assets/scripts/AI/AIMovement.cs
@@ -2,52 +2,15 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class AIMovement : MonoBehaviour
+public class AIMovement : Movement
 {
-    private float speed = AiConfig.speed;
-    private List<Transform> body;
-    private float rotationSpeed = AiConfig.rotationSpeed;
-    private float points;
-    public GameObject tailPrefab;
-    // Start is called before the first frame update
-
-    private void OnCollisionEnter(Collision collision)
-    {
-        if (collision.gameObject.tag.Equals("point"))
-        {
-            Destroy(collision.gameObject);
-            points += 1;
-            add_tail();
-        }
-        if (collision.gameObject.tag.Equals("snake"))
-        {
-            if (body.Contains(collision.transform))
-            {
-                return;
-            }
-            Debug.Log("I have died");
-            transform.position = gameController.GetRandomPosition();
-            points = 0;
-            foreach (Transform part in body)
-            {
-                Destroy(part.gameObject);
-            }
-            body = new List<Transform>();
-        }
-        if (collision.gameObject.tag.Equals("powerup"))
-        {
-
-        }
-    }
+   
 
     void Start()
     {
+        speed = AiConfig.speed;
+        rotationSpeed = AiConfig.rotationSpeed;
         body = new List<Transform>();
-    }
-
-    float DistanceTo(Vector3 first, Vector3 second)
-    {
-        return Vector3.Distance(first, second);
     }
 
     // Update is called once per frame
@@ -67,19 +30,5 @@ public class AIMovement : MonoBehaviour
             current.LookAt(previous);
             current.Translate(current.forward * speed * Time.smoothDeltaTime * 0.8f * DistanceTo(previous.position, current.position), Space.World);
         }
-    }
-
-    private void add_tail()
-    {
-        Transform newPart;
-        if (body.Count != 0)
-        {
-            newPart = Instantiate(tailPrefab as GameObject, body[body.Count - 1].position - body[body.Count - 1].forward, body[body.Count - 1].rotation).transform;
-        }
-        else
-        {
-            newPart = Instantiate(tailPrefab as GameObject, transform.position - transform.forward, transform.rotation).transform;
-        }
-        body.Add(newPart);
     }
 }
--- Assets/scripts/Config.cs
+++ Assets/scripts/Config.cs
@@ -48,7 +48,8 @@ public class Config
 {
     public static int MAP_LENGTH = 100;
     public static int MAP_WIDTH = 100;
-    public static float PLAYER_SPEED = 200f;
+    public static float PLAYER_SPEED = 15f;
+    public static float PLAYER_ROTATION = 1f;
     public static List<controls> playerControls = new List<controls> { controls.create_control(KeyCode.A, KeyCode.D), controls.create_control(KeyCode.LeftArrow, KeyCode.RightArrow), controls.create_control(KeyCode.B, KeyCode.N), controls.create_control(KeyCode.O, KeyCode.P) };
     public static List<List<view>> playerViews = new List<List<view>> {
         new List<view> { view.create_view(0, 0, 1, 1) },
