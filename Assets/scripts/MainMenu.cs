e258a74bd2e5ece69a1eef23aceb81bb9ce1bfa0
Phindile Xulu <xphindile5@gmail.com>
2019-08-27 16:54:31 +0200

    Added functionality for left input. To add functionality for other buttons

--- Assets/scripts/MainMenu.cs
+++ Assets/scripts/MainMenu.cs
@@ -3,13 +3,14 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
+using TMPro;
 public class MainMenu : MonoBehaviour
 {
     private ArrayList players;
-    public string leftControl;
-    public string rightControl;
-    public GameObject leftInputField;
-    public GameObject rigthInputField;
+    public string control;
+    // public string rightControl;
+    public TMP_InputField inputField;
+    // public TMP_InputField rigthInputField;
     public GameObject leftTextDisplay;
     public GameObject rightTextDisplay;
 
@@ -18,16 +19,20 @@ public class MainMenu : MonoBehaviour
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex +1);
     }
 
+    // public void StorePlayerDetails(string text)
     public void StorePlayerDetails()
     {
         //if (leftInputField.GetComponent<Text>().text != null)
         //{
-        Debug.Log(leftInputField.GetComponent<Text>().text);
-        //leftControl = leftInputField.GetComponent<Text>().text;
-        //leftTextDisplay.GetComponent<Text>().text = leftControl;
+        // Debug.Log(text);
+        // leftInputField.AddComponent
+        control = inputField.text;
+        Debug.Log(control);
+        Debug.Log(inputField.ToString());
+
+        // leftControl = text;
+        // leftTextDisplay.GetComponent<Text>().text = leftControl;
         //}
-
-
     }
 
 }
