56262ea22a350cfaca67956d00487ca764f0f1ca
Phindile Xulu <xphindile5@gmail.com>
2019-08-27 14:09:13 +0200

    Trying to fix null bug

--- Assets/scripts/MainMenu.cs
+++ Assets/scripts/MainMenu.cs
@@ -1,18 +1,33 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 public class MainMenu : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private ArrayList players;
+    public string leftControl;
+    public string rightControl;
+    public GameObject leftInputField;
+    public GameObject rigthInputField;
+    public GameObject leftTextDisplay;
+    public GameObject rightTextDisplay;
+
+    public void PlayGame()
     {
-        
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex +1);
     }
 
-    // Update is called once per frame
-    void Update()
+    public void StorePlayerDetails()
     {
-        
+        //if (leftInputField.GetComponent<Text>().text != null)
+        //{
+        Debug.Log(leftInputField.GetComponent<Text>().text);
+        //leftControl = leftInputField.GetComponent<Text>().text;
+        //leftTextDisplay.GetComponent<Text>().text = leftControl;
+        //}
+
+
     }
+
 }
