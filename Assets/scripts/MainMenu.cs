4d1c107e2234590425b32c5d91406ac6becffc9e
Phindile Xulu <xphindile5@gmail.com>
2019-08-28 18:20:31 +0200

    Can fetch player KeyCode

--- Assets/scripts/MainMenu.cs
+++ Assets/scripts/MainMenu.cs
@@ -8,31 +8,52 @@ public class MainMenu : MonoBehaviour
 {
     private ArrayList players;
     public string control;
-    // public string rightControl;
-    public TMP_InputField inputField;
-    // public TMP_InputField rigthInputField;
-    public GameObject leftTextDisplay;
-    public GameObject rightTextDisplay;
+    public TMP_InputField  inputField;
+    public TMP_InputField p1LInputField;
+    public TMP_InputField p1RInputField;
+    public TMP_InputField p2LInputField;
+    public TMP_InputField p2RInputField;
+    public TMP_InputField p3LInputField;
+    public TMP_InputField p3RInputField;
+    public TMP_InputField p4LInputField;
+    public TMP_InputField p4RInputField;
 
+    private void Start() {
+        // p1LInputField
+        // p1RInputField
+        // p2LInputField
+        // p2RInputField
+        // p3LInputField
+        // p3RInputField
+        // p4LInputField
+        // p4RInputField
+        // for(int i = 0; i<8;++i){
+        //     players.Add()
+        // }
+    }
     public void PlayGame()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex +1);
+        
     }
 
     // public void StorePlayerDetails(string text)
     public void StorePlayerDetails()
     {
-        //if (leftInputField.GetComponent<Text>().text != null)
-        //{
-        // Debug.Log(text);
-        // leftInputField.AddComponent
-        control = inputField.text;
-        Debug.Log(control);
-        Debug.Log(inputField.ToString());
 
-        // leftControl = text;
-        // leftTextDisplay.GetComponent<Text>().text = leftControl;
-        //}
+        // control = p1LInputField.text;
+        // Debug.Log(control);
+        
+        // Debug.Log(p1LInputField.ToString());
+        // // Debug.Log(p2LInputField.text);
+        // if(p1LInputField.ToString().Equals("Player1LeftControl (TMPro.TMP_InputField)")){
+        //     Debug.Log("Success");
+        // }
+        // // if(p2LInputField.ToString().Equals("Player1LeftControl (TMPro.TMP_InputField)")){
+        // //     Debug.Log("Success");
+        // // }
+       
+
     }
 
 }
