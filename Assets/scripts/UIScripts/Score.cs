09a71973554339f9324ca4ab509c4edc0e7752e8
Phindile Xulu <xphindile5@gmail.com>
2019-09-11 14:15:43 +0200

    Added Score UI

---
+++ Assets/scripts/UIScripts/Score.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Score : MonoBehaviour
+{
+    public GameObject player;
+    void Update()
+    {
+        
+    }
+}
