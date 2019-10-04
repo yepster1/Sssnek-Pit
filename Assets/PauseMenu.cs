958e71596e6485eb5f25d91862f5e1a5362ef244
Phindile Xulu <xphindile5@gmail.com>
2019-10-04 20:22:50 +0200

    Added functionality to pause menu

---
+++ Assets/PauseMenu.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+    public static bool gamePaused=false;
+    public GameObject pauseMenuUI;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Escape)){
+            if(gamePaused){
+                Resume();
+                Debug.Log("Resumed");
+
+            }
+            else{
+                Pause();
+                Debug.Log("Paused");
+            }
+        }
+    }
+
+    public void Resume(){
+        pauseMenuUI.SetActive(false);
+        Time.timeScale =1.0f;
+        gamePaused = false;
+    }
+    public void Pause(){
+        pauseMenuUI.SetActive(true);
+        Time.timeScale =0.0f;
+        gamePaused=true;
+    }
+
+    public void MainMenu(){
+        Time.timeScale = 1.0f;
+        SceneManager.LoadScene(0);
+    }
+    public void Quit(){
+        Application.Quit();
+    }
+}
